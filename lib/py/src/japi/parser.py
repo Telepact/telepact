f9acd0345ba8bb57549ae9c9b9d1b9bfe03ed08d
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2023-06-14 22:46:46 -0500

    wip

--- lib/py/src/japi/parser.py
+++ lib/py/src/japi/parser.py
@@ -1,12 +1,14 @@
 from typing import Any
 import json
 
+
 class Type:
     def __init__(self) -> None:
         pass
-    
+
     def getName(self):
         pass
+
 
 class JsonNull(Type):
     def __init__(self) -> None:
@@ -14,14 +16,16 @@ class JsonNull(Type):
 
     def getName(self):
         return "null"
-    
+
+
 class JsonBoolean(Type):
     def __init__(self) -> None:
         super().__init__()
 
     def getName(self):
         return "boolean"
-    
+
+
 class JsonInteger(Type):
     def __init__(self) -> None:
         super().__init__()
@@ -29,43 +33,61 @@ class JsonInteger(Type):
     def getName(self):
         return "integer"
 
+
 class JsonNumber(Type):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def getName(self):
         return "string"
 
+
+class TypeDeclaration:
+    def __init__(self, t: Type, nullable: bool) -> None:
+        self.type = t
+        self.nullable = nullable
+
+
 class JsonArray(Type):
-    def __init__(self, nestedType: TypeDeclaration) -> None:
+    def __init__(self, nested_type: TypeDeclaration) -> None:
         super().__init__()
-        self.nestedType = nestedType
-    
+        self.nested_type = nested_type
+
     def getName(self):
         return "array"
 
+
 class JsonObject(Type):
-    def __init__(self, nestedType: TypeDeclaration) -> None:
+    def __init__(self, nested_type: TypeDeclaration) -> None:
         super().__init__()
-        self.nestedType = nestedType
-    
+        self.nested_type = nested_type
+
     def getName(self):
         return "object"
 
+
+class FieldDeclaration:
+    def __init__(self, type_declaration: TypeDeclaration, optional: bool) -> None:
+        self.type_declaration = type_declaration
+        self.optional = optional
+
+
 class Struct(Type):
-    def __init__(self, name: str, fields: dict) -> None:
+    def __init__(self, name: str, fields: dict[str, FieldDeclaration]) -> None:
         super().__init__()
         self.name = name
         self.fields = fields
 
     def getName(self):
         return self.name
-    
+
+
 class Enum(Type):
-    def __init__(self, name: str, fields: dict) -> None:
+    def __init__(self, name: str, fields: dict[str, Struct]) -> None:
         super().__init__()
         self.name = name
         self.fields = fields
+
 
 class JsonAny(Type):
     def __init__(self) -> None:
@@ -73,11 +95,7 @@ class JsonAny(Type):
 
     def getName(self):
         return "any"
-    
-class TypeDeclaration:
-    def __init__(self, t: Type, nullable: bool) -> None:
-        self.type = t
-        self.nullable = nullable
+
 
 class Definition:
     def __init__(self) -> None:
@@ -86,21 +104,18 @@ class Definition:
     def getName(self):
         pass
 
-class FieldDeclaration:
-    def __init__(self, typeDeclaration: TypeDeclaration, optional: bool) -> None:
-        self.typeDeclaration = typeDeclaration
-        self.optional = optional
 
 class FunctionDefinition(Definition):
-    def __init__(self, name: str, inputStruct: Struct, outputStruct: Struct, allowedErrors: list[str]) -> None:
+    def __init__(self, name: str, input_struc: Struct, outputStruct: Struct, allowed_errors: list[str]) -> None:
         super().__init__()
         self.name = name
-        self.inputStruct = inputStruct
-        self.allowedErrors = allowedErrors
-    
+        self.input_struct = input_struc
+        self.allowed_errors = allowed_errors
+
     def getName(self):
         return self.name
-    
+
+
 class TypeDefinition(Definition):
     def __init__(self, name: str, t: Type) -> None:
         super().__init__()
@@ -110,14 +125,16 @@ class TypeDefinition(Definition):
     def getName(self):
         return self.name
 
+
 class ErrorDefinition(Definition):
     def __init__(self, name: str, fields: dict) -> None:
         super().__init__()
         self.name = name
         self.fields = fields
-    
+
     def getName(self):
         return self.name
+
 
 class TitleDefinition(Definition):
     def __init__(self, name: str) -> None:
@@ -126,19 +143,33 @@ class TitleDefinition(Definition):
 
     def getName(self):
         return self.name
-    
+
+
 class JapiParseError(Exception):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
+
 class Japi():
     def __init__(self, original: dict[str, Any], parsed: dict[str, Definition]) -> None:
         pass
-    
-def newJapi(japiAsJson: str) -> Japi:
-    parsedDefinitions = dict[str, Definition]
 
-    japiAsJsonPython = json.loads(japiAsJson)
 
-    
-    
+def newJapi(japi_as_json: str) -> Japi:
+    parsed_definitions = dict[str, Definition]
+
+    japi_as_json_python = json.loads(japi_as_json)
+
+    for definition_key, _ in japi_as_json_python:
+        if definition_key not in parsed_definitions:
+            definition = _parse_definition(japi_as_json_python)
+
+
+def _parse_definition(japi_as_json_python: dict[str, Any], parsed_definitions: dict[str, Definition], definition_key: str):
+    definition_as_json_python = japi_as_json_python[definition_key]
+    if not definition_as_json_python:
+        raise JapiParseError(
+            'could not find definition for {}'.format(definition_key))
+
+    # todo
+    pass
