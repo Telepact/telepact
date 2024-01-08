a7479d7979bf38dee9ed8a142f5a03f274d01256
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-01-08 10:30:53 -0600

    Refactor mock extension code

--- lib/java/src/main/java/io/github/brenbar/japi/MockCallTypeExtension.java
+++ lib/java/src/main/java/io/github/brenbar/japi/MockCallTypeExtension.java
@@ -19,34 +19,36 @@ public class MockCallTypeExtension implements TypeExtension {
         Map<String, Object> givenMap;
         try {
             givenMap = (Map<String, Object>) givenObj;
-
-            var optionalFunctionName = givenMap.keySet().stream().filter(k -> k.startsWith("fn.")).findAny();
-            if (optionalFunctionName.isEmpty()) {
-                validationFailures
-                        .add(new ValidationFailure(new ArrayList<Object>(), "ExtensionValidationFailure",
-                                Map.of("message", "StubMissingCall")));
-            } else {
-                var functionName = optionalFunctionName.get();
-                var functionDef = (UFn) this.types.get(functionName);
-
-                var input = (Map<String, Object>) givenMap.get(functionName);
-
-                var inputFailures = functionDef.call.values.get(functionDef.name).validate(input, List.of(), List.of());
-                var inputFailuresWithPath = inputFailures.stream()
-                        .map(f -> new ValidationFailure(_ValidateUtil.prepend(functionName, f.path), f.reason,
-                                f.data))
-                        .toList();
-                var inputFailuresWithoutMissingRequired = inputFailuresWithPath.stream()
-                        .filter(f -> !f.reason.equals("RequiredStructFieldMissing")).toList();
-
-                validationFailures.addAll(inputFailuresWithoutMissingRequired);
-            }
-
         } catch (ClassCastException e) {
-            validationFailures.add(
-                    new ValidationFailure(new ArrayList<Object>(), "ExtensionValidationFailure",
-                            Map.of("message", "CallTypeRequired")));
+            return _ValidateUtil.getTypeUnexpectedValidationFailure(new ArrayList<Object>(), givenObj, "Object");
         }
+
+        var regexString = "^fn\\..*";
+        var optionalFunctionName = givenMap.keySet().stream().filter(k -> k.matches(regexString)).findAny();
+        if (optionalFunctionName.isEmpty()) {
+            return List.of(new ValidationFailure(new ArrayList<Object>(), "RequiredObjectKeyRegexMatchFailed",
+                    Map.of("regex", regexString)));
+        }
+        var functionName = optionalFunctionName.get();
+        var functionDef = (UFn) this.types.get(functionName);
+
+        var inputInit = givenMap.get(functionName);
+        Map<String, Object> input;
+        try {
+            input = (Map<String, Object>) inputInit;
+        } catch (ClassCastException e) {
+            return _ValidateUtil.getTypeUnexpectedValidationFailure(new ArrayList<Object>(), inputInit, "Object");
+        }
+
+        var inputFailures = functionDef.call.values.get(functionDef.name).validate(input, List.of(), List.of());
+        var inputFailuresWithPath = inputFailures.stream()
+                .map(f -> new ValidationFailure(_ValidateUtil.prepend(functionName, f.path), f.reason,
+                        f.data))
+                .toList();
+        var inputFailuresWithoutMissingRequired = inputFailuresWithPath.stream()
+                .filter(f -> !f.reason.equals("RequiredStructFieldMissing")).toList();
+
+        validationFailures.addAll(inputFailuresWithoutMissingRequired);
 
         return validationFailures;
     }
--- lib/java/src/main/java/io/github/brenbar/japi/MockStubTypeExtension.java
+++ lib/java/src/main/java/io/github/brenbar/japi/MockStubTypeExtension.java
@@ -19,62 +19,72 @@ public class MockStubTypeExtension implements TypeExtension {
         Map<String, Object> givenMap;
         try {
             givenMap = (Map<String, Object>) givenObj;
-
-            var optionalFunctionName = givenMap.keySet().stream().filter(k -> k.startsWith("fn."))
-                    .findAny();
-            if (optionalFunctionName.isEmpty()) {
-                validationFailures.add(
-                        new ValidationFailure(new ArrayList<Object>(),
-                                "ExtensionValidationFailure",
-                                Map.of("message", "StubMissingCall")));
-
-            } else {
-                var functionName = optionalFunctionName.get();
-                var functionDef = (UFn) this.types.get(functionName);
-
-                var input = (Map<String, Object>) givenMap.get(functionName);
-
-                var inputFailures = functionDef.call.values.get(functionDef.name).validate(input, List.of(), List.of());
-                var inputFailuresWithPath = inputFailures.stream()
-                        .map(f -> new ValidationFailure(
-                                _ValidateUtil.prepend(functionName, f.path),
-                                f.reason,
-                                f.data))
-                        .toList();
-
-                var inputFailuresWithoutMissingRequired = inputFailuresWithPath.stream()
-                        .filter(f -> !f.reason.equals("RequiredStructFieldMissing")).toList();
-
-                validationFailures.addAll(inputFailuresWithoutMissingRequired);
-
-                if (!givenMap.containsKey("->")) {
-                    validationFailures.add(new ValidationFailure(new ArrayList<Object>(),
-                            "ExtensionValidationFailure",
-                            Map.of("message", "StubMissingResult")));
-                } else {
-
-                    var output = (Map<String, Object>) givenMap.get("->");
-
-                    var outputFailures = functionDef.result.validate(output, List.of(), List.of());
-                    var outputFailuresWithPath = outputFailures.stream()
-                            .map(f -> new ValidationFailure(
-                                    _ValidateUtil.prepend("->", f.path), f.reason,
-                                    f.data))
-                            .toList();
-                    var failuresWithoutMissingRequired = outputFailuresWithPath
-                            .stream()
-                            .filter(f -> !f.reason.equals("RequiredStructFieldMissing"))
-                            .toList();
-
-                    validationFailures.addAll(failuresWithoutMissingRequired);
-                }
-            }
-
         } catch (ClassCastException e) {
-            validationFailures.add(
-                    new ValidationFailure(new ArrayList<Object>(), "ExtensionValidationFailure",
-                            Map.of("message", "StubTypeRequired")));
+            return _ValidateUtil.getTypeUnexpectedValidationFailure(List.of(), givenObj, "Object");
         }
+
+        var regexString = "^fn\\..*";
+        var optionalFunctionName = givenMap.keySet().stream().filter(k -> k.matches(regexString))
+                .findAny();
+        if (optionalFunctionName.isEmpty()) {
+            return List.of(
+                    new ValidationFailure(List.of(),
+                            "RequiredObjectKeyRegexMatchFailed",
+                            Map.of("regex", regexString)));
+
+        }
+
+        var functionName = optionalFunctionName.get();
+        var functionDef = (UFn) this.types.get(functionName);
+
+        var inputInit = givenMap.get(functionName);
+        Map<String, Object> input;
+        try {
+            input = (Map<String, Object>) inputInit;
+        } catch (ClassCastException e) {
+            return _ValidateUtil.getTypeUnexpectedValidationFailure(List.of(), inputInit, "Object");
+        }
+
+        var inputFailures = functionDef.call.values.get(functionDef.name).validate(input, List.of(),
+                List.of());
+        var inputFailuresWithPath = inputFailures.stream()
+                .map(f -> new ValidationFailure(
+                        _ValidateUtil.prepend(functionName, f.path),
+                        f.reason,
+                        f.data))
+                .toList();
+
+        var inputFailuresWithoutMissingRequired = inputFailuresWithPath.stream()
+                .filter(f -> !f.reason.equals("RequiredStructFieldMissing")).toList();
+
+        validationFailures.addAll(inputFailuresWithoutMissingRequired);
+
+        if (!givenMap.containsKey("->")) {
+            return List.of(new ValidationFailure(List.of("->"),
+                    "RequiredObjectKeyMissing",
+                    Map.of()));
+        }
+
+        var outputInit = givenMap.get("->");
+        Map<String, Object> output;
+        try {
+            output = (Map<String, Object>) outputInit;
+        } catch (ClassCastException e) {
+            return _ValidateUtil.getTypeUnexpectedValidationFailure(List.of(), outputInit, "Object");
+        }
+
+        var outputFailures = functionDef.result.validate(output, List.of(), List.of());
+        var outputFailuresWithPath = outputFailures.stream()
+                .map(f -> new ValidationFailure(
+                        _ValidateUtil.prepend("->", f.path), f.reason,
+                        f.data))
+                .toList();
+        var failuresWithoutMissingRequired = outputFailuresWithPath
+                .stream()
+                .filter(f -> !f.reason.equals("RequiredStructFieldMissing"))
+                .toList();
+
+        validationFailures.addAll(failuresWithoutMissingRequired);
 
         return validationFailures;
     }
