00fb648e9b7a03d54c779056d876acfbb967fa26
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-01-15 14:58:59 -0600

    Refactor

--- lib/java/src/main/java/io/github/brenbar/uapi/_SerializeUtil.java
+++ lib/java/src/main/java/io/github/brenbar/uapi/_SerializeUtil.java
@@ -13,40 +13,53 @@ import java.util.TreeSet;
 class _SerializeUtil {
 
     static BinaryEncoding constructBinaryEncoding(UApiSchema uApiSchema) {
-        var allKeys = new TreeSet<String>();
-        for (var entry : uApiSchema.parsed.entrySet()) {
+        final var allKeys = new TreeSet<String>();
+        for (final var entry : uApiSchema.parsed.entrySet()) {
             allKeys.add(entry.getKey());
-            if (entry.getValue() instanceof _UStruct s) {
-                allKeys.addAll(s.fields.keySet());
-            } else if (entry.getValue() instanceof _UUnion e) {
-                for (var entry2 : e.cases.entrySet()) {
+
+            if (entry.getValue() instanceof final _UStruct s) {
+                final Map<String, _UFieldDeclaration> structFields = s.fields;
+                allKeys.addAll(structFields.keySet());
+            } else if (entry.getValue() instanceof final _UUnion u) {
+                final Map<String, _UStruct> unionCases = u.cases;
+                for (final var entry2 : unionCases.entrySet()) {
                     allKeys.add(entry2.getKey());
-                    var struct = entry2.getValue();
-                    allKeys.addAll(struct.fields.keySet());
+                    final var struct = entry2.getValue();
+                    final var structFields = struct.fields;
+                    allKeys.addAll(structFields.keySet());
                 }
-            } else if (entry.getValue() instanceof _UFn f) {
-                for (var e2 : f.call.cases.entrySet()) {
+            } else if (entry.getValue() instanceof final _UFn f) {
+                final _UUnion fnCall = f.call;
+                final Map<String, _UStruct> fnCallCases = fnCall.cases;
+                final _UUnion fnResult = f.result;
+                final Map<String, _UStruct> fnResultCases = fnResult.cases;
+
+                for (final var e2 : fnCallCases.entrySet()) {
+                    allKeys.add(e2.getKey());
+                    final var struct = e2.getValue();
+                    final Map<String, _UFieldDeclaration> structFields = struct.fields;
+                    allKeys.addAll(structFields.keySet());
+                }
+
+                for (var e2 : fnResultCases.entrySet()) {
                     allKeys.add(e2.getKey());
                     var struct = e2.getValue();
-                    allKeys.addAll(struct.fields.keySet());
-                }
-                for (var e2 : f.result.cases.entrySet()) {
-                    allKeys.add(e2.getKey());
-                    var struct = e2.getValue();
-                    allKeys.addAll(struct.fields.keySet());
+                    final Map<String, _UFieldDeclaration> structFields = struct.fields;
+                    allKeys.addAll(structFields.keySet());
                 }
             }
         }
         var i = 0;
-        var binaryEncoding = new HashMap<String, Integer>();
-        for (var key : allKeys) {
-            binaryEncoding.put(key, i++);
+        final var binaryEncoding = new HashMap<String, Integer>();
+        for (final var key : allKeys) {
+            binaryEncoding.put(key, i);
+            i += 1;
         }
-        var finalString = String.join("\n", allKeys);
-        int checksum;
+        final var finalString = String.join("\n", allKeys);
+        final int checksum;
         try {
-            var hash = MessageDigest.getInstance("SHA-256").digest(finalString.getBytes(StandardCharsets.UTF_8));
-            var buffer = ByteBuffer.wrap(hash);
+            final var hash = MessageDigest.getInstance("SHA-256").digest(finalString.getBytes(StandardCharsets.UTF_8));
+            final var buffer = ByteBuffer.wrap(hash);
             checksum = buffer.getInt();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
@@ -56,17 +69,19 @@ class _SerializeUtil {
 
     static byte[] serialize(Message message, BinaryEncoder binaryEncoder,
             SerializationImpl serializer) {
-        var headers = message.header;
+        final var headers = message.header;
 
-        boolean serializeAsBinary = false;
+        final boolean serializeAsBinary;
         if (headers.containsKey("_binary")) {
             serializeAsBinary = Objects.equals(true, headers.remove("_binary"));
+        } else {
+            serializeAsBinary = false;
         }
 
-        List<Object> messageAsPseudoJson = List.of(message.header, message.body);
+        final List<Object> messageAsPseudoJson = List.of(message.header, message.body);
         if (serializeAsBinary) {
             try {
-                var encodedMessage = binaryEncoder.encode(messageAsPseudoJson);
+                final var encodedMessage = binaryEncoder.encode(messageAsPseudoJson);
                 return serializer.toMsgPack(encodedMessage);
             } catch (BinaryEncoderUnavailableError e) {
                 // We can still submit as json
@@ -79,16 +94,17 @@ class _SerializeUtil {
 
     static Message deserialize(byte[] messageBytes, SerializationImpl serializer,
             BinaryEncoder binaryEncoder) {
-        Object messageAsPseudoJson;
-        boolean isMsgPack = false;
+        final Object messageAsPseudoJson;
+        final boolean isMsgPack;
         if (messageBytes[0] == (byte) 0x92) { // MsgPack
             isMsgPack = true;
             messageAsPseudoJson = serializer.fromMsgPack(messageBytes);
         } else {
+            isMsgPack = false;
             messageAsPseudoJson = serializer.fromJson(messageBytes);
         }
 
-        List<Object> messageAsPseudoJsonList;
+        final List<Object> messageAsPseudoJsonList;
         try {
             messageAsPseudoJsonList = _CastUtil.asList(messageAsPseudoJson);
         } catch (ClassCastException e) {
@@ -99,7 +115,7 @@ class _SerializeUtil {
             throw new DeserializationError("ExpectedArrayOfTwoObjects");
         }
 
-        List<Object> finalMessageAsPseudoJsonList;
+        final List<Object> finalMessageAsPseudoJsonList;
         if (isMsgPack) {
             try {
                 finalMessageAsPseudoJsonList = binaryEncoder.decode(messageAsPseudoJsonList);
