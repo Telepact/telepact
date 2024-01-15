87a968096f47645a622d9ccc412f7d089e9aba07
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-01-14 22:42:15 -0600

    Refactor client util

--- lib/java/src/main/java/io/github/brenbar/uapi/_ClientUtil.java
+++ lib/java/src/main/java/io/github/brenbar/uapi/_ClientUtil.java
@@ -10,28 +10,28 @@ import java.util.function.BiFunction;
 class _ClientUtil {
 
     static Message processRequestObject(Message requestMessage,
-            BiFunction<Message, Serializer, Future<Message>> adapter, Serializer serializer, long defaultTimeoutMs,
-            boolean defaultBinary) {
+            BiFunction<Message, Serializer, Future<Message>> adapter, Serializer serializer, long timeoutMsDefault,
+            boolean useBinaryDefault) {
         try {
             if (!requestMessage.header.containsKey("_tim")) {
-                requestMessage.header.put("_tim", defaultTimeoutMs);
+                requestMessage.header.put("_tim", timeoutMsDefault);
             }
 
-            if (defaultBinary) {
+            if (useBinaryDefault) {
                 requestMessage.header.put("_binary", true);
             }
 
-            var timeoutMs = ((Number) requestMessage.header.get("_tim")).longValue();
+            final var timeoutMs = ((Number) requestMessage.header.get("_tim")).longValue();
 
-            var responseMessage = adapter.apply(requestMessage, serializer).get(timeoutMs,
-                    TimeUnit.MILLISECONDS);
+            final var responseMessage = adapter.apply(requestMessage, serializer).get(timeoutMs, TimeUnit.MILLISECONDS);
 
             if (Objects.equals(responseMessage.body,
                     Map.of("_ErrorParseFailure",
                             Map.of("reasons", List.of(Map.of("IncompatibleBinaryEncoding", Map.of())))))) {
                 // Try again, but as json
                 requestMessage.header.put("_forceSendJson", true);
-                responseMessage = adapter.apply(requestMessage, serializer).get(timeoutMs,
+
+                return adapter.apply(requestMessage, serializer).get(timeoutMs,
                         TimeUnit.MILLISECONDS);
             }
 
