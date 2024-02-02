d7e74069683e3ddea7007d1c4bf8d5262a844008
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-02-02 12:26:51 -0600

    wip: fixing java code coverage

--- qa/lib/java/build_test.py
+++ qa/lib/java/build_test.py
@@ -9,7 +9,6 @@ this_env = os.environ.copy()
 def start(nats_url):
     this_env['NATS_URL'] = nats_url
 
-    p = subprocess.Popen(['mvn', 'compile', 'exec:java',
-                         '-Dexec.mainClass=uapitest.Main'], cwd=this_path, env=this_env)
+    p = subprocess.Popen(['mvn', 'verify'], cwd=this_path, env=this_env)
 
     return p
