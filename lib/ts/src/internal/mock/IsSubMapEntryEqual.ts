3ae2e95daff9a0972df46355977d62e50eb639ad
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-06-25 15:19:25 -0500

    ts: tix mock matcher

--- lib/ts/src/internal/mock/IsSubMapEntryEqual.ts
+++ lib/ts/src/internal/mock/IsSubMapEntryEqual.ts
@@ -1,18 +1,21 @@
 import { isSubMap } from '../../internal/mock/IsSubMap';
 import { partiallyMatches } from '../../internal/mock/PartiallyMatches';
+import { objectsAreEqual } from '../ObjectsAreEqual';
 
 export function isSubMapEntryEqual(partValue: any, wholeValue: any): boolean {
-    if (typeof partValue === 'object' && typeof wholeValue === 'object') {
-        return isSubMap(partValue, wholeValue);
-    } else if (Array.isArray(partValue) && Array.isArray(wholeValue)) {
-        for (const partElement of partValue) {
+    if (Array.isArray(partValue) && Array.isArray(wholeValue)) {
+        for (let i = 0; i < partValue.length; i += 1) {
+            const partElement = partValue[i];
             const partMatches = partiallyMatches(wholeValue, partElement);
             if (!partMatches) {
                 return false;
             }
         }
+
         return true;
+    } else if (typeof partValue === 'object' && typeof wholeValue === 'object') {
+        return isSubMap(partValue, wholeValue);
     } else {
-        return partValue === wholeValue;
+        return objectsAreEqual(partValue, wholeValue);
     }
 }
