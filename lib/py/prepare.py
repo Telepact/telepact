d8b41160352b2bd8ddb100df10f8dc672429131d
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2024-12-31 09:02:44 -0600

    wip: auth py tests passing

--- lib/py/prepare.py
+++ lib/py/prepare.py
@@ -2,3 +2,4 @@ import shutil
 
 shutil.copy('../../common/internal.uapi.json', 'uapi')
 shutil.copy('../../common/mock-internal.uapi.json', 'uapi')
+shutil.copy('../../common/auth.uapi.json', 'uapi')
