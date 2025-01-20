e789eebfd2a045437da76a1f1afc19d3bbad6127
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-01-20 13:35:35 -0600

    fix uapi naming

--- lib/ts/src/internal/schema/CreateUApiSchemaFromFileJsonMap.ts
+++ lib/ts/src/internal/schema/CreateUApiSchemaFromFileJsonMap.ts
@@ -1,22 +1,22 @@
-import { getInternalUApiJson } from '../../internal/schema/GetInternalUApiJson';
-import { getAuthUApiJson } from '../../internal/schema/GetAuthUApiJson';
-import { UApiSchema } from '../../UApiSchema';
-import { parseUapiSchema } from '../../internal/schema/ParseUApiSchema';
+import { getInternalUApiJson } from "../../internal/schema/GetInternalUApiJson";
+import { getAuthUApiJson } from "../../internal/schema/GetAuthUApiJson";
+import { UApiSchema } from "../../UApiSchema";
+import { parseUApiSchema } from "../../internal/schema/ParseUApiSchema";
 
 export function createUApiSchemaFromFileJsonMap(jsonDocuments: Record<string, string>): UApiSchema {
     const finalJsonDocuments = { ...jsonDocuments };
-    finalJsonDocuments['internal_'] = getInternalUApiJson();
+    finalJsonDocuments["internal_"] = getInternalUApiJson();
 
     // Determine if we need to add the auth schema
     for (const json of Object.values(jsonDocuments)) {
         const regex = /"struct\.Auth_"\s*:/;
         if (regex.test(json)) {
-            finalJsonDocuments['auth_'] = getAuthUApiJson();
+            finalJsonDocuments["auth_"] = getAuthUApiJson();
             break;
         }
     }
 
-    const uApiSchema = parseUapiSchema(finalJsonDocuments);
+    const uApiSchema = parseUApiSchema(finalJsonDocuments);
 
     return uApiSchema;
 }
