56d015fb0eb8308d1228e5d8cbd8119995a5cb06
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-01-25 23:06:22 -0600

    add codegen to py test build

--- lib/py/uapi/internal/HandleMessage.py
+++ lib/py/uapi/internal/HandleMessage.py
@@ -23,6 +23,8 @@ async def handle_message(
     from uapi.internal.types.UFn import UFn
     from uapi.internal.validation.ValidateContext import ValidateContext
 
+    print("Handling message")
+
     response_headers: dict[str, object] = {}
     request_headers: dict[str, object] = request_message.headers
     request_body: dict[str, object] = request_message.body
