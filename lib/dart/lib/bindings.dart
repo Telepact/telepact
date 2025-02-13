ce4942d72080b632174c9372e1600efb8b538bb8
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-02-13 17:02:39 -0600

    wip: dart

--- lib/dart/lib/bindings.dart
+++ lib/dart/lib/bindings.dart
@@ -1,5 +1,6 @@
 import 'package:node_interop/node.dart';
 import 'package:node_interop/util.dart';
+import 'dart:typed_data';
 
 final dynamic _npmPackage = require('uapi');
 
@@ -7,7 +8,11 @@ class RandomGenerator {
   final dynamic _instance;
 
   RandomGenerator(int collectionLengthMin, int collectionLengthMax)
-      : _instance = callConstructor(getProperty(_npmPackage, 'RandomGenerator'), [collectionLengthMin, collectionLengthMax]);
+      : _instance =
+            callConstructor(getProperty(_npmPackage, 'RandomGenerator'), [
+          collectionLengthMin,
+          collectionLengthMax,
+        ]);
 
   void setSeed(int seed) {
     callMethod(_instance, 'setSeed', [seed]);
@@ -42,19 +47,25 @@ class Checksum {
   final dynamic _instance;
 
   Checksum(String value, int expiration)
-      : _instance = callConstructor(getProperty(_npmPackage, 'Checksum'), [value, expiration]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'Checksum'), [
+          value,
+          expiration,
+        ]);
 }
 
 abstract class ClientBinaryStrategy {
-    void updateChecksum(String newChecksum);
-    dynamic getCurrentChecksums();
+  void updateChecksum(String newChecksum);
+  dynamic getCurrentChecksums();
 }
 
 class DefaultClientBinaryStrategy extends ClientBinaryStrategy {
   final dynamic _instance;
 
   DefaultClientBinaryStrategy()
-      : _instance = callConstructor(getProperty(_npmPackage, 'DefaultClientBinaryStrategy'), []);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'DefaultClientBinaryStrategy'),
+          [],
+        );
 
   void updateChecksum(String newChecksum) {
     callMethod(_instance, 'updateChecksum', [newChecksum]);
@@ -66,17 +77,20 @@ class DefaultClientBinaryStrategy extends ClientBinaryStrategy {
 }
 
 abstract class Serialization {
-    Uint8List toJson(dynamic uapiMessage);
-    Uint8List toMsgpack(dynamic uapiMessage);
-    dynamic fromJson(Uint8List bytes);
-    dynamic fromMsgpack(Uint8List bytes);
+  Uint8List toJson(dynamic uapiMessage);
+  Uint8List toMsgpack(dynamic uapiMessage);
+  dynamic fromJson(Uint8List bytes);
+  dynamic fromMsgpack(Uint8List bytes);
 }
 
 class DefaultSerialization extends Serialization {
   final dynamic _instance;
 
   DefaultSerialization()
-      : _instance = callConstructor(getProperty(_npmPackage, 'DefaultSerialization'), []);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'DefaultSerialization'),
+          [],
+        );
 
   Uint8List toJson(dynamic uapiMessage) {
     return callMethod(_instance, 'toJson', [uapiMessage]);
@@ -99,7 +113,10 @@ class Serializer {
   final dynamic _instance;
 
   Serializer(DefaultSerialization serializationImpl, dynamic binaryEncoder)
-      : _instance = callConstructor(getProperty(_npmPackage, 'Serializer'), [serializationImpl._instance, binaryEncoder]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'Serializer'), [
+          serializationImpl._instance,
+          binaryEncoder,
+        ]);
 
   Uint8List serialize(dynamic message) {
     return callMethod(_instance, 'serialize', [message]);
@@ -114,7 +131,10 @@ class Client {
   final dynamic _instance;
 
   Client(dynamic adapter, dynamic options)
-      : _instance = callConstructor(getProperty(_npmPackage, 'Client'), [adapter, options]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'Client'), [
+          adapter,
+          options,
+        ]);
 
   Future<dynamic> request(dynamic requestMessage) {
     return callMethod(_instance, 'request', [requestMessage]);
@@ -125,7 +145,11 @@ class Server {
   final dynamic _instance;
 
   Server(dynamic uApiSchema, dynamic handler, dynamic options)
-      : _instance = callConstructor(getProperty(_npmPackage, 'Server'), [uApiSchema, handler, options]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'Server'), [
+          uApiSchema,
+          handler,
+          options,
+        ]);
 
   Future<Uint8List> process(Uint8List requestMessageBytes) {
     return callMethod(_instance, 'process', [requestMessageBytes]);
@@ -136,7 +160,10 @@ class MockServer {
   final dynamic _instance;
 
   MockServer(dynamic mockUApiSchema, dynamic options)
-      : _instance = callConstructor(getProperty(_npmPackage, 'MockServer'), [mockUApiSchema, options]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'MockServer'), [
+          mockUApiSchema,
+          options,
+        ]);
 
   Future<Uint8List> process(Uint8List message) {
     return callMethod(_instance, 'process', [message]);
@@ -148,7 +175,10 @@ class Message {
   final dynamic _instance;
 
   Message(dynamic headers, dynamic body)
-      : _instance = callConstructor(getProperty(_npmPackage, 'Message'), [headers, body]);
+      : _instance = callConstructor(getProperty(_npmPackage, 'Message'), [
+          headers,
+          body,
+        ]);
 
   dynamic getBodyTarget() {
     return callMethod(_instance, 'getBodyTarget', []);
@@ -164,26 +194,52 @@ class ValidationFailure {
   final dynamic _instance;
 
   ValidationFailure(dynamic path, String reason, dynamic data)
-      : _instance = callConstructor(getProperty(_npmPackage, 'ValidationFailure'), [path, reason, data]);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'ValidationFailure'),
+          [path, reason, data],
+        );
 }
 
 // Bindings for UApiSchema class
 class UApiSchema {
   final dynamic _instance;
 
-  UApiSchema(dynamic original, dynamic full, dynamic parsed, dynamic parsedRequestHeaders, dynamic parsedResponseHeaders)
-      : _instance = callConstructor(getProperty(_npmPackage, 'UApiSchema'), [original, full, parsed, parsedRequestHeaders, parsedResponseHeaders]);
+  UApiSchema(
+    dynamic original,
+    dynamic full,
+    dynamic parsed,
+    dynamic parsedRequestHeaders,
+    dynamic parsedResponseHeaders,
+  ) : _instance = callConstructor(getProperty(_npmPackage, 'UApiSchema'), [
+          original,
+          full,
+          parsed,
+          parsedRequestHeaders,
+          parsedResponseHeaders,
+        ]);
 
   static UApiSchema fromJson(dynamic json) {
-    return UApiSchema(callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromJson', [json]));
+    return UApiSchema(
+      callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromJson', [json]),
+    );
   }
 
   static UApiSchema fromFileJsonMap(dynamic fileJsonMap) {
-    return UApiSchema(callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromFileJsonMap', [fileJsonMap]));
+    return UApiSchema(
+      callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromFileJsonMap', [
+        fileJsonMap,
+      ]),
+    );
   }
 
   static UApiSchema fromDirectory(String directory, dynamic fs, dynamic path) {
-    return UApiSchema(callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromDirectory', [directory, fs, path]));
+    return UApiSchema(
+      callMethod(getProperty(_npmPackage, 'UApiSchema'), 'fromDirectory', [
+        directory,
+        fs,
+        path,
+      ]),
+    );
   }
 }
 
@@ -191,27 +247,64 @@ class UApiSchema {
 class UApiSchemaParseError {
   final dynamic _instance;
 
-  UApiSchemaParseError(List<dynamic> schemaParseFailures, Map<String, dynamic> documentNamesToJson, dynamic cause)
-      : _instance = callConstructor(getProperty(_npmPackage, 'UApiSchemaParseError'), [schemaParseFailures, documentNamesToJson, cause]);
+  UApiSchemaParseError(
+    List<dynamic> schemaParseFailures,
+    Map<String, dynamic> documentNamesToJson,
+    dynamic cause,
+  ) : _instance = callConstructor(
+          getProperty(_npmPackage, 'UApiSchemaParseError'),
+          [schemaParseFailures, documentNamesToJson, cause],
+        );
 }
 
 // Bindings for MockUApiSchema class
 class MockUApiSchema {
   final dynamic _instance;
 
-  MockUApiSchema(dynamic original, dynamic full, dynamic parsed, dynamic parsedRequestHeaders, dynamic parsedResponseHeaders)
-      : _instance = callConstructor(getProperty(_npmPackage, 'MockUApiSchema'), [original, full, parsed, parsedRequestHeaders, parsedResponseHeaders]);
+  MockUApiSchema(
+    dynamic original,
+    dynamic full,
+    dynamic parsed,
+    dynamic parsedRequestHeaders,
+    dynamic parsedResponseHeaders,
+  ) : _instance = callConstructor(getProperty(_npmPackage, 'MockUApiSchema'), [
+          original,
+          full,
+          parsed,
+          parsedRequestHeaders,
+          parsedResponseHeaders,
+        ]);
 
   static MockUApiSchema fromJson(dynamic json) {
-    return MockUApiSchema(callMethod(getProperty(_npmPackage, 'MockUApiSchema'), 'fromJson', [json]));
+    return MockUApiSchema(
+      callMethod(getProperty(_npmPackage, 'MockUApiSchema'), 'fromJson', [
+        json,
+      ]),
+    );
   }
 
   static MockUApiSchema fromFileJsonMap(dynamic jsonDocuments) {
-    return MockUApiSchema(callMethod(getProperty(_npmPackage, 'MockUApiSchema'), 'fromFileJsonMap', [jsonDocuments]));
+    return MockUApiSchema(
+      callMethod(
+        getProperty(_npmPackage, 'MockUApiSchema'),
+        'fromFileJsonMap',
+        [jsonDocuments],
+      ),
+    );
   }
 
-  static MockUApiSchema fromDirectory(String directory, dynamic fs, dynamic path) {
-    return MockUApiSchema(callMethod(getProperty(_npmPackage, 'MockUApiSchema'), 'fromDirectory', [directory, fs, path]));
+  static MockUApiSchema fromDirectory(
+    String directory,
+    dynamic fs,
+    dynamic path,
+  ) {
+    return MockUApiSchema(
+      callMethod(getProperty(_npmPackage, 'MockUApiSchema'), 'fromDirectory', [
+        directory,
+        fs,
+        path,
+      ]),
+    );
   }
 }
 
@@ -220,7 +313,12 @@ class UApiSchemaFiles {
   final dynamic _instance;
 
   UApiSchemaFiles(String directory, dynamic fs, dynamic path)
-      : _instance = callConstructor(getProperty(_npmPackage, 'UApiSchemaFiles'), [directory, fs, path]);
+      : _instance =
+            callConstructor(getProperty(_npmPackage, 'UApiSchemaFiles'), [
+          directory,
+          fs,
+          path,
+        ]);
 }
 
 // Bindings for BinaryEncoderUnavailableError class
@@ -228,7 +326,10 @@ class BinaryEncoderUnavailableError {
   final dynamic _instance;
 
   BinaryEncoderUnavailableError(String message)
-      : _instance = callConstructor(getProperty(_npmPackage, 'BinaryEncoderUnavailableError'), [message]);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'BinaryEncoderUnavailableError'),
+          [message],
+        );
 }
 
 // Bindings for SerializationError class
@@ -236,7 +337,10 @@ class SerializationError {
   final dynamic _instance;
 
   SerializationError(dynamic cause)
-      : _instance = callConstructor(getProperty(_npmPackage, 'SerializationError'), [cause]);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'SerializationError'),
+          [cause],
+        );
 }
 
 // Bindings for UApiError class
@@ -244,7 +348,8 @@ class UApiError {
   final dynamic _instance;
 
   UApiError(dynamic arg)
-      : _instance = callConstructor(getProperty(_npmPackage, 'UApiError'), [arg]);
+      : _instance =
+            callConstructor(getProperty(_npmPackage, 'UApiError'), [arg]);
 }
 
 // Bindings for MockServerOptions class
@@ -252,7 +357,10 @@ class MockServerOptions {
   final dynamic _instance;
 
   MockServerOptions()
-      : _instance = callConstructor(getProperty(_npmPackage, 'MockServerOptions'), []);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'MockServerOptions'),
+          [],
+        );
 }
 
 // Bindings for ServerOptions class
@@ -260,7 +368,10 @@ class ServerOptions {
   final dynamic _instance;
 
   ServerOptions()
-      : _instance = callConstructor(getProperty(_npmPackage, 'ServerOptions'), []);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'ServerOptions'),
+          [],
+        );
 }
 
 // Bindings for ClientOptions class
@@ -268,5 +379,30 @@ class ClientOptions {
   final dynamic _instance;
 
   ClientOptions()
-      : _instance = callConstructor(getProperty(_npmPackage, 'ClientOptions'), []);
+      : _instance = callConstructor(
+          getProperty(_npmPackage, 'ClientOptions'),
+          [],
+        );
+
+  bool get useBinary => getProperty(_instance, 'useBinary');
+  set useBinary(bool value) => setProperty(_instance, 'useBinary', value);
+
+  bool get alwaysSendJson => getProperty(_instance, 'alwaysSendJson');
+  set alwaysSendJson(bool value) =>
+      setProperty(_instance, 'alwaysSendJson', value);
+
+  int get timeoutMsDefault => getProperty(_instance, 'timeoutMsDefault');
+  set timeoutMsDefault(int value) =>
+      setProperty(_instance, 'timeoutMsDefault', value);
+
+  DefaultSerialization get serializationImpl => DefaultSerialization()
+    .._instance = getProperty(_instance, 'serializationImpl');
+  set serializationImpl(DefaultSerialization value) =>
+      setProperty(_instance, 'serializationImpl', value._instance);
+
+  DefaultClientBinaryStrategy get binaryStrategy =>
+      DefaultClientBinaryStrategy()
+        .._instance = getProperty(_instance, 'binaryStrategy');
+  set binaryStrategy(DefaultClientBinaryStrategy value) =>
+      setProperty(_instance, 'binaryStrategy', value._instance);
 }
