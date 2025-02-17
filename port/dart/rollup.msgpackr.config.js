8c5175b6740db0494ad09a42208fccfb09005747
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-02-16 23:19:25 -0600

    ci updates dart

--- port/dart/rollup.msgpackr.config.js
+++ port/dart/rollup.msgpackr.config.js
@@ -1,12 +1,29 @@
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
+import fs from "fs";
+import path from "path";
+
+const license = fs
+    .readFileSync(path.resolve("node_modules/msgpackr/LICENSE"), "utf-8")
+    .split("\n")
+    .map((line) => `    ${line}`)
+    .join("\n");
 
 export default {
-    input: 'node_modules/msgpackr/index.js', // Entry point for the dependency
+    input: "node_modules/msgpackr/index.js", // Entry point for the dependency
     output: {
-        file: 'dist/msgpackr.iife.js',
-        format: 'iife',
-        name: 'msgpackr',
+        file: "dist/msgpackr.iife.js",
+        format: "iife",
+        name: "msgpackr",
         sourcemap: true,
+        banner: `/*
+
+${license}
+*/
+
+/*
+    This file is a Rollup redistribution of the original \`msgpackr\` npm package.
+*/
+`,
     },
     plugins: [commonjs()],
 };
