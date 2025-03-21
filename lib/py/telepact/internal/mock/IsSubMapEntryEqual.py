bfc061516aee7b6401a96f1eb1d26d7e5ab769fa
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-03-21 10:08:42 -0500

    Add license header

--- lib/py/telepact/internal/mock/IsSubMapEntryEqual.py
+++ lib/py/telepact/internal/mock/IsSubMapEntryEqual.py
@@ -1,3 +1,19 @@
+#|
+#|  Copyright The Telepact Authors
+#|
+#|  Licensed under the Apache License, Version 2.0 (the "License");
+#|  you may not use this file except in compliance with the License.
+#|  You may obtain a copy of the License at
+#|
+#|  https://www.apache.org/licenses/LICENSE-2.0
+#|
+#|  Unless required by applicable law or agreed to in writing, software
+#|  distributed under the License is distributed on an "AS IS" BASIS,
+#|  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#|  See the License for the specific language governing permissions and
+#|  limitations under the License.
+#|
+
 def is_sub_map_entry_equal(part_value: object, whole_value: object) -> bool:
     from ...internal.mock.IsSubMap import is_sub_map
     from ...internal.mock.PartiallyMatches import partially_matches
