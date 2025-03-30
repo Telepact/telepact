4d07b3fb591ad18f64dde269d3381402d1500367
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-03-29 22:21:29 -0500

    Update banners

--- bind/dart/rollup.crc32.config.js
+++ bind/dart/rollup.crc32.config.js
@@ -24,6 +24,8 @@ export default {
         name: "crc32",
         sourcemap: true,
         banner: `/*
+    Copyright (C) 2014-present   SheetJS LLC
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
