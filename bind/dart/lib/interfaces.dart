36615f9edbedfeee510937c88f136044a6a28aa7
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-03-30 09:58:08 -0500

    Format code

--- bind/dart/lib/interfaces.dart
+++ bind/dart/lib/interfaces.dart
@@ -24,7 +24,7 @@ class TelepactSchema {
   final b.TelepactSchema telepactSchema;
 
   /// Constructs a [TelepactSchema] from a binding schema.
-  TelepactSchema(b.TelepactSchema schema): telepactSchema = schema;
+  TelepactSchema(b.TelepactSchema schema) : telepactSchema = schema;
 
   /// Creates a [TelepactSchema] from a JSON string.
   static TelepactSchema fromJson(String json) {
@@ -56,7 +56,7 @@ class Message {
 
   /// Maps JavaScript body to Dart body.
   static mapJsBody(JSAny body) {
-    final b = body.dartify()as Map<Object?, Object?>;
+    final b = body.dartify() as Map<Object?, Object?>;
     return b.cast<String, Object>();
   }
 
@@ -75,14 +75,15 @@ class Serializer {
   final b.Serializer _serializer;
 
   /// Constructs a [Serializer] from a JavaScript binding serializer.
-  Serializer.fromJS(b.Serializer serializer)
-      : _serializer = serializer;
+  Serializer.fromJS(b.Serializer serializer) : _serializer = serializer;
 
   /// Serializes a [Message] into a [Uint8List].
-  Uint8List serialize(Message message) => _serializer.serialize(message._message).toDart;
+  Uint8List serialize(Message message) =>
+      _serializer.serialize(message._message).toDart;
 
   /// Deserializes a [Uint8List] into a [Message].
-  Message deserialize(Uint8List messageBytes) => Message.fromJS(_serializer.deserialize(messageBytes.toJS));
+  Message deserialize(Uint8List messageBytes) =>
+      Message.fromJS(_serializer.deserialize(messageBytes.toJS));
 }
 
 /// A client for sending requests and receiving responses.
@@ -90,19 +91,23 @@ class Client {
   final b.Client _client;
 
   /// Constructs a [Client] with an adapter function and client options.
-  Client(Future<Message> Function(Message m, Serializer s) adapter, ClientOptions options)
+  Client(Future<Message> Function(Message m, Serializer s) adapter,
+      ClientOptions options)
       : _client = initClient(adapter, options);
 
   /// Initializes the client with the adapter function and options.
-  static b.Client initClient(Future<Message> Function(Message im, Serializer iis) adapter, ClientOptions options) {
-      JSPromise outerAdapter(b.Message om, b.Serializer os) {
-        final m = Message.fromJS(om);
-        final s = Serializer.fromJS(os);
-        return adapter(m, s).then((response) {
-          return response._message;
-        }).toJS;
-      }
-      return b.Client(outerAdapter.toJS, options._options);
+  static b.Client initClient(
+      Future<Message> Function(Message im, Serializer iis) adapter,
+      ClientOptions options) {
+    JSPromise outerAdapter(b.Message om, b.Serializer os) {
+      final m = Message.fromJS(om);
+      final s = Serializer.fromJS(os);
+      return adapter(m, s).then((response) {
+        return response._message;
+      }).toJS;
+    }
+
+    return b.Client(outerAdapter.toJS, options._options);
   }
 
   /// Sends a request message and receives a response message.
@@ -117,8 +122,7 @@ class ClientOptions {
   final b.ClientOptions _options;
 
   /// Constructs default [ClientOptions].
-  ClientOptions()
-      : _options = b.ClientOptions();
+  ClientOptions() : _options = b.ClientOptions();
 
   /// Sets whether to use binary serialization.
   set useBinary(bool value) => _options.useBinary = value;
@@ -130,7 +134,8 @@ class ClientOptions {
   set timeoutMsDefault(int value) => _options.timeoutMsDefault = value;
 
   /// Sets the namespace for local storage cache.
-  set localStorageCacheNamespace(String value) => _options.localStorageCacheNamespace = value;
+  set localStorageCacheNamespace(String value) =>
+      _options.localStorageCacheNamespace = value;
 }
 
 /// A server for processing requests and sending responses.
@@ -138,18 +143,21 @@ class Server {
   final b.Server _server;
 
   /// Constructs a [Server] with a schema, handler function, and server options.
-  Server(TelepactSchema schema, Future<Message> Function(Message m) handler, ServerOptions options)
+  Server(TelepactSchema schema, Future<Message> Function(Message m) handler,
+      ServerOptions options)
       : _server = initServer(schema.telepactSchema, handler, options);
 
   /// Initializes the server with the schema, handler function, and options.
-  static b.Server initServer(b.TelepactSchema schema, Future<Message> Function(Message im) handler, ServerOptions options) {
-      JSPromise outerHandler(b.Message om) {
-        final m = Message.fromJS(om);
-        return handler(m).then((response) {
-          return response._message;
-        }).toJS;
-      }
-      return b.Server(schema, outerHandler.toJS, options._options);
+  static b.Server initServer(b.TelepactSchema schema,
+      Future<Message> Function(Message im) handler, ServerOptions options) {
+    JSPromise outerHandler(b.Message om) {
+      final m = Message.fromJS(om);
+      return handler(m).then((response) {
+        return response._message;
+      }).toJS;
+    }
+
+    return b.Server(schema, outerHandler.toJS, options._options);
   }
 
   /// Processes a request message and returns the response as a [Uint8List].
@@ -164,17 +172,18 @@ class ServerOptions {
   final b.ServerOptions _options;
 
   /// Constructs default [ServerOptions].
-  ServerOptions()
-      : _options = b.ServerOptions();
+  ServerOptions() : _options = b.ServerOptions();
 
   /// Sets the error handler function.
   set onError(void Function(JSObject e) value) => _options.onError = value.toJS;
 
   /// Sets the request handler function.
-  set onRequest(void Function(JSObject) value) => _options.onRequest = value.toJS;
+  set onRequest(void Function(JSObject) value) =>
+      _options.onRequest = value.toJS;
 
   /// Sets the response handler function.
-  set onResponse(void Function(JSObject) value) => _options.onResponse = value.toJS;
+  set onResponse(void Function(JSObject) value) =>
+      _options.onResponse = value.toJS;
 
   /// Sets whether authentication is required.
   set authRequired(bool value) => _options.authRequired = value;
@@ -188,40 +197,39 @@ class MockServerOptions {
   final b.MockServerOptions _options;
 
   /// Constructs default [MockServerOptions].
-  MockServerOptions()
-      : _options = b.MockServerOptions();
+  MockServerOptions() : _options = b.MockServerOptions();
 
   /// Sets the error handler function.
-  /// 
+  ///
   /// [value] is a function that handles errors represented as [JSObject].
   set onError(void Function(JSObject e) value) => _options.onError = value.toJS;
 
   /// Enables or disables message response generation.
-  /// 
+  ///
   /// [value] determines whether message response generation is enabled.
   set enableMessageResponseGeneration(bool value) =>
       _options.enableMessageResponseGeneration = value;
 
   /// Enables or disables optional field generation.
-  /// 
+  ///
   /// [value] determines whether optional field generation is enabled.
   set enableOptionalFieldGeneration(bool value) =>
       _options.enableOptionalFieldGeneration = value;
 
   /// Randomizes optional field generation.
-  /// 
+  ///
   /// [value] determines whether optional field generation is randomized.
   set randomizeOptionalFieldGeneration(bool value) =>
       _options.randomizeOptionalFieldGeneration = value;
 
   /// Sets the minimum length for generated collections.
-  /// 
+  ///
   /// [value] specifies the minimum length for generated collections.
   set generatedCollectionLengthMin(int value) =>
       _options.generatedCollectionLengthMin = value;
 
   /// Sets the maximum length for generated collections.
-  /// 
+  ///
   /// [value] specifies the maximum length for generated collections.
   set generatedCollectionLengthMax(int value) =>
       _options.generatedCollectionLengthMax = value;
@@ -232,12 +240,12 @@ class MockTelepactSchema {
   final b.MockTelepactSchema _schema;
 
   /// Constructs a [MockTelepactSchema] from a binding schema.
-  /// 
+  ///
   /// [schema] is the binding schema to wrap.
   MockTelepactSchema(b.MockTelepactSchema schema) : _schema = schema;
 
   /// Creates a [MockTelepactSchema] from a JSON string.
-  /// 
+  ///
   /// [json] is the JSON string representing the schema.
   static MockTelepactSchema fromJson(String json) {
     final schema = b.MockTelepactSchema.fromJson(json);
@@ -250,14 +258,14 @@ class MockServer {
   final b.MockServer _server;
 
   /// Constructs a [MockServer] with a mock schema and options.
-  /// 
+  ///
   /// [schema] is the mock schema to use.
   /// [options] are the options for configuring the mock server.
   MockServer(MockTelepactSchema schema, MockServerOptions options)
       : _server = b.MockServer(schema._schema, options._options);
 
   /// Processes a request message and returns the response as a [Uint8List].
-  /// 
+  ///
   /// [messageBytes] is the request message in bytes.
   /// Returns the response message as a [Uint8List].
   Future<Uint8List> process(Uint8List messageBytes) async {
--- bind/dart/lib/src/bindings.dart
+++ bind/dart/lib/src/bindings.dart
@@ -19,7 +19,6 @@ library;
 
 import 'dart:js_interop';
 
-
 extension type TelepactSchema._(JSObject _) implements JSObject {
   external JSArray get original;
   external JSArray get full;
@@ -58,8 +57,7 @@ extension type Serializer._(JSObject _) implements JSObject {
 }
 
 extension type Client._(JSObject _) implements JSObject {
-  external factory Client(
-      JSFunction adapter, ClientOptions options);
+  external factory Client(JSFunction adapter, ClientOptions options);
   external JSPromise<Message> request(Message requestMessage);
 }
 
@@ -69,7 +67,7 @@ extension type ClientOptions._(JSObject _) implements JSObject {
   external int get timeoutMsDefault;
   external Serialization get serializationImpl;
   external String get localStorageCacheNamespace;
-  
+
   external void set useBinary(bool value);
   external void set alwaysSendJson(bool value);
   external void set timeoutMsDefault(int value);
@@ -97,11 +95,9 @@ extension type ServerOptions._(JSObject _) implements JSObject {
   external void set onResponse(JSFunction onResponse);
   external void set authRequired(bool authRequired);
   external void set serialization(Serialization serialization);
-  
 
   external factory ServerOptions();
 }
-
 
 extension type MockServer._(JSObject _) implements JSObject {
   external factory MockServer(JSAny mockTelepactSchema, JSAny options);
@@ -138,4 +134,3 @@ extension type TelepactSchemaParseError._(JSObject _) implements JSObject {
 extension type SerializationError._(JSObject _) implements JSObject {}
 
 extension type TelepactError._(JSObject _) implements JSObject {}
-
