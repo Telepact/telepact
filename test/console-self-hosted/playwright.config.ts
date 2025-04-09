0054275d5eac33d16282821dd9e1c59368df40b1
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-04-09 10:25:24 -0500

    Fix self-host test

--- test/console-self-hosted/playwright.config.ts
+++ test/console-self-hosted/playwright.config.ts
@@ -14,14 +14,25 @@
 //|  limitations under the License.
 //|
 
-import { defineConfig, devices } from '@playwright/test';
-import { spawn } from 'child_process';
+import { defineConfig } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const VERSION = fs.readFileSync(path.resolve(__dirname, '../../VERSION.txt'), 'utf-8').trim();
 
 export default defineConfig({
   testDir: './tests',
   timeout: 60 * 1000, // 1 minute
-  webServer: {
-    command: 'npm run serve',
-    port: 5173
-  },
+	webServer: [
+		{
+			command: `docker run --name telepact_console_self_host_test -p 8082:8080 telepact-console-self-host:${VERSION}`,
+			port: 8082,
+		}
+	],
+  globalTeardown: path.resolve(__dirname, 'teardown.js')
 });
--- test/console-self-hosted/tests/example.spec.ts
+++ test/console-self-hosted/tests/example.spec.ts
@@ -17,6 +17,6 @@
 import { test, expect } from '@playwright/test';
 
 test('should display "Telepact" on page load', async ({ page }) => {
-  await page.goto('http://localhost:5173'); // Update URL if necessary
+  await page.goto('http://localhost:8082'); // Update URL if necessary
   await expect(page.locator('text=Telepact')).toBeVisible();
 });
