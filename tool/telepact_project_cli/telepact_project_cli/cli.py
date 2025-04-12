793c6e5b358e81b25afac593aac1e78e430d7590
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-04-11 19:19:19 -0500

    Fix poetry file scanning

--- tool/telepact_project_cli/telepact_project_cli/cli.py
+++ tool/telepact_project_cli/telepact_project_cli/cli.py
@@ -38,106 +38,6 @@ def main() -> None:
 
 
 @click.command()
-def old_bump() -> None:
-    if os.path.exists("pom.xml"):
-        parser = ET.XMLParser(remove_blank_text=True)
-        tree = ET.parse("pom.xml", parser)
-        root = tree.getroot()
-        version = root.find("{http://maven.apache.org/POM/4.0.0}version").text
-        new_version = bump_version(version)
-        root.find(
-            "{http://maven.apache.org/POM/4.0.0}version").text = new_version
-        tree.write("pom.xml", xml_declaration=True,
-                   encoding='utf-8', pretty_print=True)
-        click.echo(f"Updated pom.xml to version {new_version}")
-    elif os.path.exists("package.json"):
-        with open("package.json", "r") as f:
-            data = json.load(f)
-        version = data["version"]
-        new_version = bump_version(version)
-        data["version"] = new_version
-        with open("package.json", "w") as f:
-            json.dump(data, f, indent=2)
-        click.echo(f"Updated package.json to version {new_version}")
-    elif os.path.exists("pyproject.toml"):
-        with open("pyproject.toml", "r") as f:
-            data = toml.load(f)
-        version = data["project"]["version"]
-        new_version = bump_version(version)
-        data["project"]["version"] = new_version
-        with open("pyproject.toml", "w") as f:
-            toml.dump(data, f)
-        click.echo(f"Updated pyproject.toml to version {new_version}")
-    elif os.path.exists("pubspec.yaml"):
-        with open("pubspec.yaml", "r") as f:
-            data = yaml.load(f)
-        version = data["version"]
-        new_version = bump_version(version)
-        data["version"] = new_version
-        with open("pubspec.yaml", "w") as f:
-            yaml.dump(data, f)
-        click.echo(f"Updated pubspec.yaml to version {new_version}")
-    else:
-        click.echo("No supported project file found.")
-
-
-@click.command()
-@click.argument('version')
-def depset(version: str) -> None:
-    if os.path.exists("pom.xml"):
-        parser = ET.XMLParser(remove_blank_text=True)
-        tree = ET.parse("pom.xml", parser)
-        root = tree.getroot()
-        dependencies = root.find(
-            "{http://maven.apache.org/POM/4.0.0}dependencies")
-        for dependency in dependencies.findall("{http://maven.apache.org/POM/4.0.0}dependency"):
-            artifact_id = dependency.find(
-                "{http://maven.apache.org/POM/4.0.0}artifactId").text
-            if artifact_id == "telepact":
-                dependency.find(
-                    "{http://maven.apache.org/POM/4.0.0}version").text = version
-                break
-        tree.write("pom.xml", xml_declaration=True,
-                   encoding='utf-8', pretty_print=True)
-        click.echo(f"Set telepact dependency to version {version} in pom.xml")
-    elif os.path.exists("package.json"):
-        with open("package.json", "r") as f:
-            data = json.load(f)
-        if "dependencies" in data and "telepact" in data["dependencies"]:
-            data["dependencies"]["telepact"] = version
-            with open("package.json", "w") as f:
-                json.dump(data, f, indent=2)
-            click.echo(
-                f"Set telepact dependency to version {version} in package.json")
-        else:
-            click.echo("telepact dependency not found in package.json")
-    elif os.path.exists("pyproject.toml"):
-        with open("pyproject.toml", "r") as f:
-            data = toml.load(f)
-        if "dependencies" in data["project"] and "telepact" in data["project"]["dependencies"]:
-            data["project"]["dependencies"]["telepact"] = version
-            with open("pyproject.toml", "w") as f:
-                toml.dump(data, f)
-            click.echo(
-                f"Set telepact dependency to version {version} in pyproject.toml")
-        else:
-            click.echo("telepact dependency not found in pyproject.toml")
-    elif os.path.exists("pubspec.yaml"):
-        with open("pubspec.yaml", "r") as f:
-            data = yaml.load(f)
-        if "dependencies" in data and "telepact" in data["dependencies"]:
-            data["dependencies"]["telepact"] = version
-            with open("pubspec.yaml", "w") as f:
-                yaml.dump(data, f)
-            click.echo(
-                f"Set telepact dependency to version {version} in pubspec.yaml")
-        else:
-            click.echo("telepact dependency not found in pubspec.yaml")
-    else:
-        click.echo("No supported project file found.")
-
-
-@click.command()
 def get() -> None:
     if os.path.exists("pom.xml"):
         parser = ET.XMLParser(remove_blank_text=True)
@@ -153,7 +53,7 @@ def get() -> None:
     elif os.path.exists("pyproject.toml"):
         with open("pyproject.toml", "r") as f:
             data = toml.load(f)
-        version = data["project"]["version"]
+        version = data["tool"]['poetry']["version"]
         click.echo(version, nl=False)
     elif os.path.exists("pubspec.yaml"):
         with open("pubspec.yaml", "r") as f:
@@ -277,7 +177,7 @@ def bump() -> None:
             elif project_file.endswith("pyproject.toml"):
                 with open(project_file, 'r') as f:
                     data = toml.load(f)
-                data["project"]["version"] = new_version
+                data["tool"]['poetry']["version"] = new_version
                 with open(project_file, 'w') as f:
                     toml.dump(data, f)
                 click.echo(f"Updated {project_file} to version {new_version}")
@@ -628,8 +528,6 @@ def release() -> None:
     except Exception as e:
         click.echo(f"Failed to create release or upload assets: {e}")
 
-main.add_command(old_bump)
-main.add_command(depset)
 main.add_command(get)
 main.add_command(set_version)
 main.add_command(bump)
