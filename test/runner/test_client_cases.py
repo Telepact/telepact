57ff0574c135b3d791874c06fd7d5d3facb955a4
Brendan Bartels <12563144+brenbar@users.noreply.github.com>
2025-10-09 22:15:51 -0500

    Update tests

--- test/runner/test_client_cases.py
+++ test/runner/test_client_cases.py
@@ -18,10 +18,10 @@ cases = {
     'simple': [
         [[{'@setSeed': 0, '@expectedPseudoJsonBody': {'Ok_': {}}}, {'fn.ping_': {}}], [{}, {'Ok_': {}}]],
         [[{'@expectMatch': False,'@expectedPseudoJsonBody': {'Ok_': {}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{'@assertionError': True}, {'ErrorUnknown_': {}}]],
-        [[{'@expectedPseudoJsonBody': {'Ok_': {}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{}, {'Ok_': {}}]],
-        [[{'@expectedPseudoJsonBody': {'Ok_': {'value!': {}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
-        [[{'@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
-        [[{'@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
+        [[{'@expectMatch': True, '@expectedPseudoJsonBody': {'Ok_': {}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{}, {'Ok_': {}}]],
+        [[{'@expectMatch': True, '@expectedPseudoJsonBody': {'Ok_': {'value!': {}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
+        [[{'@expectMatch': True, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
+        [[{'@expectMatch': True, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
 
         # The required field is missing and will be filled in with a random value, since a match is not expected.
         [[{'@expectMatch': False, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': False, 'optional!': True}}}}]],
@@ -30,6 +30,6 @@ cases = {
         [[{'@expectMatch': False, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
         [[{'@expectMatch': False, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {}}}, {'fn.test': {}}], [{}, {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}]],
 
-        [[{'@expectMatch': False, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True, 'optional!': True}}}}}, {'fn.test': {}}], [{'@assertionError': True}, {'ErrorUnknown_': {}}]],
+        [[{'@expectMatch': True, '@expectedPseudoJsonBody': {'Ok_': {'value!': {'struct!': {'optional!': True}}}}, '@result': {'Ok_': {'value!': {'struct!': {'required': True}}}}}, {'fn.test': {}}], [{'@assertionError': True}, {'ErrorUnknown_': {}}]],
     ],
 }
